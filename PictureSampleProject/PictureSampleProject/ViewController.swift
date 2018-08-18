539f18088c7c93d3a1f15523beb39f20659b3a81
kyo_s <kyosyunn@yahoo.co.jp>
2018-08-18 14:09:10 +0900

    update picture sample project

--- PictureSampleProject/PictureSampleProject/ViewController.swift
+++ PictureSampleProject/PictureSampleProject/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     }
 
     @IBOutlet weak var imageView: UIImageView!
-
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var imageName: UITextField!
+    @IBOutlet weak var imageSizeLabel: UILabel!
+    @IBOutlet weak var resultImageSizeLabel: UILabel!
 
     @IBOutlet weak var loadImageButton: UIButton!
 
@@ -25,7 +28,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func sampleButtonTapped(_ sender: UIButton) {
+    // MARK: -- カメラロールから写真の読み込み
+    @IBAction func selectPhotoFromLibrary(_ sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             // 写真を選ぶビュー
             let pickerView = UIImagePickerController()
@@ -37,24 +41,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             // ビューに表示
             self.present(pickerView, animated: true)
         }
-
-//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
-//
-//            let picker = UIImagePickerController()
-//            picker.modalPresentationStyle = UIModalPresentationStyle.popover
-//            picker.delegate = self // UINavigationControllerDelegate と　UIImagePickerControllerDelegateを実装する
-//            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-//
-//            if let popover = picker.popoverPresentationController {
-//                popover.sourceView = self.view
-//                popover.sourceRect = loadImageButton.frame // ポップオーバーの表示元となるエリア
-//                popover.permittedArrowDirections = UIPopoverArrowDirection.any
-//            }
-//            self.present(picker, animated: true, completion: nil)
-//        }
     }
 
-    @IBAction func sampleButton2Tapped(_ sender: UIButton) {
+    // MARK: -- カメラからの写真の読み込み。
+    @IBAction func selectPhotoFromCamera(_ sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             // 写真を選ぶビュー
             let pickerView = UIImagePickerController()
@@ -66,15 +56,109 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             // ビューに表示
             self.present(pickerView, animated: true)
         }
-
     }
+
+    //MARK: -- 写真をローカルのディレクトリに保存する
+    @IBAction func savePhotoToLocalDirectory(_ sender: UIButton) {
+        guard let image = imageView.image else {
+            let vc = createSimpleAlertDialg(title: "確認", message: "imageを選択してください")
+            self.present(vc, animated: true, completion: nil)
+            return
+        }
+
+        guard let imageName = imageName.text, imageName != "" else {
+            let vc = createSimpleAlertDialg(title: "確認", message: "image名を入力してください")
+            self.present(vc, animated: true, completion: nil)
+            return
+        }
+
+        if let data = UIImagePNGRepresentation(image) {
+            // ディレクトリにファイル名を追加する
+            let filename = getDocumentsDirectory().appendingPathComponent(imageName)
+            try? data.write(to: filename)
+        }
+    }
+
+    // ファイルを保存するディレクトリを取得
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+
+    //MARK: -- 写真をローカルディレクトリから読み込む
+    @IBAction func locadPhotoFromLocalDirectory(_ sender: UIButton) {
+        guard let imageName = imageName.text, imageName != "" else {
+            let vc = createSimpleAlertDialg(title: "確認", message: "image名を入力してください")
+            self.present(vc, animated: true, completion: nil)
+            return
+        }
+
+        let filename = getDocumentsDirectory().appendingPathComponent(imageName)
+        resultImageView.image = UIImage(contentsOfFile: filename.path)
+        resultImageSizeLabel.text = getSizeOfUIImage(image: resultImageView.image!)
+    }
+
+    //MARK: -- 表示している写真を圧縮する
+    @IBAction func resizePhoto(_ sender: UIButton) {
+        guard let image = imageView.image else {
+            self.present(createSimpleAlertDialg(title: "確認", message: "imageを選択してください"), animated: true)
+            return
+        }
+        let resizeSize = CGSize(width: image.size.width / 5 , height: image.size.height / 5)
+        resultImageView.image = resize(image: image, size: resizeSize)
+        resultImageSizeLabel.text = getSizeOfUIImage(image: resultImageView.image!)
+    }
+
+    func resize(image: UIImage, size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / image.size.width
+        let heightRatio = _size.height / image.size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+
+        let resizedSize = CGSize(width: image.size.width * ratio, height: image.size.height * ratio)
+
+        UIGraphicsBeginImageContext(resizedSize)
+        image.draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return resizedImage
+    }
+
+    //MARK: -- UIImageの容量を取得する
+    func getSizeOfUIImage(image: UIImage) -> String {
+        if let data = UIImagePNGRepresentation(image) {
+            // ディレクトリにファイル名を追加する
+            let filename = getDocumentsDirectory().appendingPathComponent("fileNameForGetSizeOfUIImage.png")
+            try? data.write(to: filename)
+            do {
+                let attributes = try FileManager.default.attributesOfItem(atPath: filename.path)
+                return "\(attributes[.size] ?? 0)"
+            } catch {
+                print("error")
+            }
+        }
+        return "0"
+    }
+
 }
 
 extension ViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = pickedImage
+            imageSizeLabel.text = getSizeOfUIImage(image: pickedImage)
         }
         picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: -- util
+extension ViewController {
+    func createSimpleAlertDialg(title: String, message: String) -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        return alertController
     }
 }
