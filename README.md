9405e12e64a19c08070977ff76f0bddeb5bf05cd
kyo_s <kyosyunn@yahoo.co.jp>
2018-08-21 01:48:10 +0900

    update samplewebview2

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 |プロジェクト|概要|ポイント|
 |---|---|---|
 |PictureSampleProject|カメラロール・写真撮影した内容を表示する|- カメラからの写真の読み込み。<br> - カメラロールから写真の読み込み <br> - 写真をローカルのディレクトリ(documentDirectory)に保存する <br> - 写真をローカルディレクトリから読み込む<br> - 表示している写真を圧縮する <br> - UIImageの容量を取得する|
-|sampleWebViewProject|ローカルのWebViewを表示する|- ローカルのHTMLファイルを読み込んでWKWebViewに表示する|
+|sampleWebViewProject|ローカルのWebViewを表示する| - ローカルのHTMLファイルを読み込んでWKWebViewに表示する<br> - SwiftからJS操作を行う。<br> - SwiftからWebViewにデータを受け渡す<br>|
+|sampleWebViewProject|webViewの画像処理| - 画像をUIImageをBASE64Encodeして、WebViewに受け渡す|
 |||||
--- sampleWebViewProject2/sampleWebViewProject2/ViewController.swift
+++ sampleWebViewProject2/sampleWebViewProject2/ViewController.swift
@@ -12,42 +12,20 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
-    @IBOutlet weak var nameTextField: UITextField!
     var image = #imageLiteral(resourceName: "SamplePic")
-//    var saveUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("assessmentPicture.png")
-    var saveUrl = Bundle.main.bundleURL.appendingPathComponent("assessmentPicture.png")
 
     override func viewDidLoad() {
+        //ローカルのHTMLの読み込み
         let html = try? String(contentsOfFile: Bundle.main.path(forResource: "index", ofType: "html")!)
-//        let baseUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let baseUrl = Bundle.main.bundleURL.appendingPathComponent("assessmentPicture.png")
         webView.loadHTMLString(html!, baseURL: baseUrl)
-        print("baseUrl: \(baseUrl)")
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    @IBAction func saveImage(_ sender: UIButton) {
-        let pngImageData = UIImagePNGRepresentation(image)
-        try? pngImageData?.write(to: saveUrl, options: Data.WritingOptions.atomic)
-        print("saveUrl: \(saveUrl.absoluteString)")
-    }
-
-    @IBAction func changeText(_ sender: UIButton) {
-        webView.evaluateJavaScript("window.hello('\(nameTextField.text!)')", completionHandler: nil)
-    }
-
+    // MARK: -- UIImageをBASE64Encodeして、WebViewに受け渡す
     @IBAction func setImage(_ sender: UIButton) {
-//        webView.evaluateJavaScript("window.setImage('\(saveUrl.path)')", completionHandler: nil)
-//        webView.evaluateJavaScript("window.setImage('\(saveUrl.path)')", completionHandler: nil)
         webView.evaluateJavaScript("window.setImage('data:image/png;base64,\(base64Encode(image: image))')", completionHandler: nil)
-        print("loadImagePath: \(saveUrl.path)")
     }
 
     func base64Encode(image: UIImage) -> String {
