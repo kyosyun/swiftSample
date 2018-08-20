31ac98237900de4fda54775ca7193dfc64591dd0
kyo_s <kyosyunn@yahoo.co.jp>
2018-08-21 00:24:37 +0900

    update webview sample

--- sampleWebViewProject/sampleWebViewProject/ViewController.swift
+++ sampleWebViewProject/sampleWebViewProject/ViewController.swift
@@ -11,14 +11,12 @@ import WebKit
 
 class ViewController: UIViewController {
 
-
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var nameTextField: UITextField!
 
     //外部URLへのアクセス
 //    var url = URLRequest(url: URL(string: "https://developer.apple.com/")!)
     //内部リソースへのアクセス
-    var urlString = Bundle.main.path(forResource: "index", ofType: "html")!
     var image = #imageLiteral(resourceName: "SamplePic.png")
     var imageName = "assessmentPicture.png"
 
@@ -26,20 +24,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loadAddressURL()
         saveImage(image: image, path: fileInDocumentsDirectory(fileName: imageName))
-
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func saveImage() {
         let pngImageData = UIImagePNGRepresentation(image)
         let savePath = Bundle.main.bundlePath + "/temp/res.png"
         let saveUrl = URL(string: savePath)
-        let result = try? pngImageData?.write(to: saveUrl!, options: Data.WritingOptions.atomic)
+        try? pngImageData?.write(to: saveUrl!, options: Data.WritingOptions.atomic)
     }
 
     func saveImage(image: UIImage, path: URL) {
@@ -47,10 +38,27 @@ class ViewController: UIViewController {
         try? pngImageData?.write(to: path)
     }
 
+    //MARK: -- ローカルのHTMLファイルを読み込みWebViewに表示する
     func loadAddressURL() {
-        //ローカルのHTMLへのアクセスを行う
+        let urlString = Bundle.main.path(forResource: "index", ofType: "html")!
         let html = try? String(contentsOfFile: urlString)
         webView.loadHTMLString(html!, baseURL: Bundle.main.bundleURL)
+    }
+
+    //MARK: -- SwiftからJS操作を行う。
+    @IBAction func btnTapped(_ sender: UIButton) {
+        webView.evaluateJavaScript("window.test()", completionHandler: nil)
+    }
+
+    //MARK: -- SwiftからWebViewにデータを受け渡す
+    @IBAction func hello(_ sender: UIButton) {
+        webView.evaluateJavaScript("window.hello('\(nameTextField.text!)')", completionHandler: nil)
+    }
+
+    //MARK: -- SwiftからJS操作を行って画像をセットする
+    @IBAction func setImage(_ sender: UIButton) {
+        let savePath = fileInDocumentsDirectory(fileName: imageName).path
+        webView.evaluateJavaScript("window.setImage('\(savePath)')", completionHandler: nil)
     }
 
     func getDocumentsURL() -> URL {
@@ -61,17 +69,5 @@ class ViewController: UIViewController {
         return getDocumentsURL().appendingPathComponent(fileName)
     }
 
-    @IBAction func btnTapped(_ sender: UIButton) {
-        webView.evaluateJavaScript("window.test()", completionHandler: nil)
-    }
-
-    @IBAction func hello(_ sender: UIButton) {
-        webView.evaluateJavaScript("window.hello('\(nameTextField.text!)')", completionHandler: nil)
-    }
-
-    @IBAction func setImage(_ sender: UIButton) {
-        let savePath = fileInDocumentsDirectory(fileName: imageName).path
-        webView.evaluateJavaScript("window.setImage('\(savePath)')", completionHandler: nil)
-    }
 }
 
