f54fbfc472817ce0cffa3edfa46e34e19cab4d72
kyo_s <kyosyunn@yahoo.co.jp>
2019-02-19 12:12:33 +0900

    add[growthPushSample] first commit

---
+++ GrowthPushSample/GrowthPushSample-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  GrowthPushSample-Bridging-Header.h
+//  GrowthPushSample
+//
+//  Created by 許　駿 on 2019/02/08.
+//  Copyright © 2019年 kyo_s. All rights reserved.
+//
+
+#import <Growthbeat/Growthbeat.h>
+
+#ifndef GrowthPushSample_Bridging_Header_h
+#define GrowthPushSample_Bridging_Header_h
+
+
+#endif /* GrowthPushSample_Bridging_Header_h */
---
+++ GrowthPushSample/GrowthPushSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrowthPushSample
+//
+//  Created by 許　駿 on 2019/02/08.
+//  Copyright © 2019年 kyo_s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
