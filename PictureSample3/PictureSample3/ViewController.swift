4c1ce8b2d55c28b5b689b7bfc705c210a0081a56
kyo_s <kyosyunn@yahoo.co.jp>
2019-08-25 23:59:53 +0900

    add[PictureSample3] first commit

---
+++ PictureSample3/PictureSample3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureSample3
+//
+//  Created by 許　駿 on 2019/08/25.
+//  Copyright © 2019年 kyo_s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
