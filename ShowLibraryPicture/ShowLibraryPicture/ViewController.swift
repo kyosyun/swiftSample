673cca656246e07c57fbada86f6421548cf50898
kyo_s <kyosyunn@yahoo.co.jp>
2020-04-16 23:34:56 +0900

    写真をライブラリーから読み込む

---
+++ ShowLibraryPicture/ShowLibraryPicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowLibraryPicture
+//
+//  Created by 許　駿 on 2020/03/07.
+//  Copyright © 2020 kyo_s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
