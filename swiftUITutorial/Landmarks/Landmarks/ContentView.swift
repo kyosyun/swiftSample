ebb76e84b7f7ee2a1a2ac99ce2f242053baffb5a
kyo_s <kyosyunn@yahoo.co.jp>
2021-09-12 16:45:04 +0900

    swiftUI Tutorial Viewの生成の結合

--- swiftUITutorial/Landmarks/Landmarks/ContentView.swift
+++ swiftUITutorial/Landmarks/Landmarks/ContentView.swift
@@ -10,17 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack (alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.black)
-            HStack {
+        VStack () {
+            MapView().frame(height: 300).ignoresSafeArea(edges: .top)
+            CircleImage().offset(y: -130).padding(.bottom, -130)
+            VStack (alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
                     Text("Joshua Tree National Park.")
-                        .font(.subheadline)
+                    Spacer()
                     Text("California")
-                        .font(.subheadline)
-
-            }
+                }.font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                VStack(alignment: .leading) {
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Descriptive text goes here.")
+                }
+            }.padding()
+            Spacer()
         }
     }
 }
